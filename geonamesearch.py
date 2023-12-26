8efab28389f9f325d3842b073bf2cfb9c362bbf4
Natalia <144121898+NataliaKulikovskaya@users.noreply.github.com>
2023-12-26 22:35:07 +0300

    Add files via upload

--- geonamesearch.py
+++ geonamesearch.py
@@ -1,24 +1,19 @@
 import pandas as pd
 import numpy as np
-
+import os
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine.url import URL
 from sqlalchemy.exc import SQLAlchemyError
-
 from transliterate import slugify
 from transliterate import detect_language
-
 from thefuzz import process
-import numpy as np
-
 from dotenv import load_dotenv
-import os
 
 engine = None
 
-assert load_dotenv()  # Raises AssertionError if .env is not there
+assert load_dotenv()
 
-# Take the data from the .env file
+# Получаем данные из файла .env
 USR = os.getenv("USR")
 PWD = os.getenv("PWD")
 DB_HOST = os.getenv("DB_HOST")
@@ -35,16 +30,16 @@ DATABASE = {
     "query": {},
 }
 
-# Creating an Engine object
+# Создание объекта Engine
 engine = create_engine(URL.create(**DATABASE))
 
-# Checking the connetion
+# Проверка соединения
 try:
     with engine.connect() as conn:
-        # Trying to execute a simple test query. The `text` function converst a string into and SQL-query
+        # Попытка выполнить простой тестовый запрос. Функция `text` преобразует строку в SQL-запрос
         result = conn.execute(text("SELECT 1"))
         for _ in result:
-            pass  # don't do anything
+            pass
     print(f"Connection established: {DATABASE['database']} на {DATABASE['host']}")
 except SQLAlchemyError as e:
     print(f"Connection error: {e}")
@@ -53,14 +48,12 @@ except SQLAlchemyError as e:
 def load_the_data(
     country_selection=["RU", "KZ", "AM", "RS", "ME", "KG", "GE"], expanded=False
 ):
-    """Loads the vectors of geonameIDs, names and alternatenaes from the database. Reformats the data into the dictionary compatible with `thefuzz`"""
     if expanded:
         table = "geonames"
     else:
         table = "cities15000"
     where_clause = ""
     if country_selection:
-        # country_selection = list(country_selection)
         if len(country_selection) == 1:
             where_clause = f"WHERE country_code = '{country_selection[0]}'"
         else:
@@ -71,10 +64,8 @@ def load_the_data(
     FROM {table}  
     {where_clause}
     """
-    # print(query)
-    # print(country_selection, len(country_selection))
 
-    # Load the data into a pandas DataFrame
+    # Загружаем данные
     global engine
     df = pd.read_sql_query(query, con=engine, index_col="geonameid")
 
@@ -83,13 +74,8 @@ def load_the_data(
 
     for i in range(len(altnames)):
         altnames[i].append(names[i])
-    # Creating the dictionary of the structure geonameID: names for all cities including official and alternative names
-    # d = {
-    #     zip(df.index, altnames)
-    # }  # does same as
-    d = {ind: n for ind, n in zip(df.index, altnames)}
+     d = {ind: n for ind, n in zip(df.index, altnames)}
     return d
-
 
 def search(
     query: str,
@@ -98,68 +84,50 @@ def search(
     asdict=True,
     country_selection=["RU", "KZ", "AM", "RS", "ME", "KG", "GE"],
 ):
-    """The rapid fuzzy search for city names. The matches are defined based
-    on the Levenshtein distance. The function takes a query string and returns
-    k best matches. Supports parabolic and exponential weighting.
 
-    Parameters:
-    - query (str): the query string
-    - k (int, optional): Desired number of matches
-    - weight_mode={None, 'sq', 'exp'} (str, optional):
-        * None: do not weight closer matches
-        * 'sq': apply parabolic weighting to the similarity scores
-        * 'exp': apply exponential weighting to the similarity scores
-    - asdict (bool, optional): whether to convert the result into a python dict. If False, returns Pandas DataFrame
-    - country_selection (tuple): Customize the area of search. Takes ISO country codes. Does NOT handle exceptions!
-    Set to None to searh around the globe.
-
-    Returns:
-    - pd.DataFrame | dict: A DataFrame or dictionary the top matching cities,
-      including geonameid as index, name, country name and the region.
-      Sorted by the matching score in descending order.
-    """
     global engine
 
     d = load_the_data(country_selection)
 
     if detect_language(query) is not None:
         query = slugify(query)
-    scores = {}  # container for match scores for each city
+    scores = {}
 
     for (
         ind,
         name_list,
     ) in (
         d.items()
-    ):  ## for each city calculate similarity scores with every alternative name
+    ):  # для каждого города вычислим оценки схожести со всеми альтернативными названиями
+    _ = np.array(process.extract(query, name_list))
         _ = np.array(process.extract(query, name_list))
 
         if weight_mode == "exp":
-            # Calculate the scores and weighting them exponentially
+            # Вычислим оценки и взвесить их экспоненциально
             scores[ind] = np.exp(_[:, 1].astype(int)).sum() / len(
                 _
-            )  # sum up the exponents of the scores and normalize by the number of possible names
+            )   # суммируемь экспоненты оценок и нормализовать по числу возможных названий
             scores[ind] = np.log(scores[ind])  # return to the readable values
         elif weight_mode == "sq":
-            # Calculate the scores and weighting them parabolically
+            # Вычислим оценки и взвесить их параболически
             scores[ind] = np.square(_[:, 1].astype(int)).sum() / len(_)
             scores[ind] = np.sqrt(scores[ind])
-        else:  # @TODO: check for the actually None value?
+        else:
             scores[ind] = _[:, 1].astype(int).sum() / len(_)
 
-    # sorted by the matching score (.2 ms faster with the native Python function than with Pandas df.sort_values) see tests below
+
+# отсортировано по score
     scores_df = pd.DataFrame.from_records(
         sorted(scores.items(), key=lambda item: item[1], reverse=True),
         columns=["geonameid", "score"],
     )
     scores_df.loc[:, "score"] = scores_df.loc[:, "score"].round(
         3
-    )  # so this looks nicer
+    )
 
     indexes = tuple(
         scores_df.loc[: k - 1, "geonameid"]
-    )  # select the DataFrame indicies of the top k
-    # print(indexes)
+    )  # выбираем индексы DataFrame для первых k значений
 
     query = f"""
         SELECT DISTINCT
